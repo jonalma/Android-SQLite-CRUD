4cc1e3c412dc91c3e3e5b660159ce4b69fd67539
Jon <jonialmazan@gmail.com>
2017-03-13 06:26:21 -0700

    first commit

--- app/src/main/java/com/androidhive/androidsqlite/AndroidSQLiteTutorialActivity.java
+++ app/src/main/java/com/androidhive/androidsqlite/AndroidSQLiteTutorialActivity.java
@@ -5,17 +5,18 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
 public class AndroidSQLiteTutorialActivity extends Activity {
+    DatabaseHandler db = new DatabaseHandler(this);
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        DatabaseHandler db = new DatabaseHandler(this);
-        
+
         /**
          * CRUD Operations
          * */
@@ -28,15 +29,29 @@ public class AndroidSQLiteTutorialActivity extends Activity {
  
         // Reading all contacts
         Log.d("Reading: ", "Reading all contacts..");
-        List<Contact> contacts = db.getAllContacts();       
- 
+        outputAllContacts();
+
+        // Delete all contacts
+        final Button deleteAllContactsButton = (Button) findViewById(R.id.deleteAllContacts);
+        deleteAllContactsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                db.removeAll();
+                outputAllContacts();
+            }
+        });
+    }//end onCreate
+
+    public void outputAllContacts(){
+        List<Contact> contacts = db.getAllContacts();
+        final TextView allContacts = (TextView) findViewById(R.id.allContactsTV);
+        allContacts.setText("Currently in SQLite: \n");
+
         for (Contact cn : contacts) {
             String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
-                // Writing Contacts to log
-        Log.d("Name: ", log);
-        
-        }
+            // Writing Contacts to log
+            Log.d("Name: ", log);
 
-        //db.removeAll();
+            allContacts.append(log + "\n");
+        }
     }
 }
